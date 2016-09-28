48dce5ff7433e8de8c8c9a34e068ad492c8358b1
Ethan <shuyouxin1@gmail.com>
2016-09-28 14:28:44 +0800

    add password verify

--- book/book.py
+++ book/book.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, url_for, render_template, request, flash, redirect
+from flask import Blueprint, url_for, render_template, request, flash, \
+    redirect, make_response, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_httpauth import HTTPBasicAuth
+
 
 book_bp = Blueprint(
     'book', 
@@ -8,7 +12,21 @@ book_bp = Blueprint(
     template_folder='../templates',
 )
 
+auth = HTTPBasicAuth()
+
 books = ['The Name of the Rose', 'The Historian', 'Rebecca']
+users = [
+    {'username': 'ethan', 'password': generate_password_hash('6689')},
+    {'username': 'peter', 'password': generate_password_hash('4567')}
+]
+
+@auth.verify_password
+def verify_password(username, password):
+    for user in users:
+        if user['username'] == username:
+            if check_password_hash(user['password'], password):
+                return True
+    return False
 
 
 @book_bp.route('/', methods=['GET'])
@@ -16,20 +34,30 @@ def index():
     return '<h1>Hello World!</h1>'
 
 
-@book_bp.route('/book', methods=['GET', 'POST'])
-def handle_book():
-    _form = request.form
-
-    if request.method == 'POST':
-        title = _form["title"]
-        books.append(title)
-        flash("add book successfully!")
-        return redirect(url_for('book.handle_book'))
-
+@book_bp.route('/book', methods=['GET'])
+def get_books():
     return render_template(
         'book.html',
         books=books
-   )
+    )
+
+
+@book_bp.route('/book', methods=['POST'])
+@auth.login_required
+def add_book():
+    _form = request.form
+    title = _form["title"]
+    if not title:
+        return '<h1>invalid request</h1>'
+
+    books.append(title)
+    flash("add book successfully!")
+    return redirect(url_for('book.get_books'))
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 
 @book_bp.route('/book/<name>')
@@ -42,4 +70,3 @@ def get_book_info(name):
         'book.html',
         books=book
     )
-
