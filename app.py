beb189f0f8f8958b7c7997b91fbc90711e577a99
Ethan <shuyouxin1@gmail.com>
2016-10-06 19:38:10 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
+from configs import config
 from book import book_bp
 from movie import movie_bp
 
-app = Flask(__name__)
-app.secret_key = 'The quick brown fox jumps over the lazy dog'
 
-app.register_blueprint(book_bp)
-app.register_blueprint(movie_bp)
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # basic config
+    app.config.from_object(config[config_name])
+
+    # blueprints
+    app.register_blueprint(book_bp)
+    app.register_blueprint(movie_bp)
+
+    # error handler
+    handle_errors(app)
+
+    return app
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
-
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5200, debug=True)
+def handle_errors(app):
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html'), 404
