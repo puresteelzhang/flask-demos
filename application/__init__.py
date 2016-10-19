c77117626778769176a0759db2c7fd43ceb673dc
Ethan <shuyouxin1@gmail.com>
2016-10-19 10:50:25 +0800

    add app.py

---
+++ application/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from app import create_app
+from app import db
+
+__all__ = ['create_app', 'db']
--- application/app.py
+++ application/app.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 from controllers import blueprints
 from configs import config
-from extensions import db
+from extensions import db, login_manager
 
 
 def create_app(config_name=None):
@@ -16,8 +16,23 @@ def create_app(config_name=None):
     # db
     db.init_app(app)
 
+    # login
+    login_manager.init_app(app)
+
     # blueprints
     for bp in blueprints:
         app.register_blueprint(bp)
 
+    handle_errors(app)
+
     return app
+
+
+def handle_errors(app):
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(403)
+    def acess_forbidden_error(error):
+        return render_template('403.html'), 403
