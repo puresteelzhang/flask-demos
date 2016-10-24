6ffcda03d64fd4f84e2a890f2bfaba47c91bbf55
Ethan <shuyouxin1@gmail.com>
2016-10-24 09:48:03 +0800

    add extensions.py

--- application/extensions.py
+++ application/extensions.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+# db
 db = SQLAlchemy()
+
+# login_manager
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
